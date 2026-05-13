7c845a884db49008cea05fd54dbb4c2e87f6e425
Juan Camilo Fernández S. <juan.fernandez@sistemas aries.com.co>
2026-05-12 20:41:42 -0500

    hero

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from 'react-router-dom';
-import heroImg from '../assets/hero.png';
+import heroImg from '../assets/Hero.png';
 
 export default function Home() {
   const navigate = useNavigate();
